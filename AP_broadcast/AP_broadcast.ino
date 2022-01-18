a9b7e21202d121ab893f4b7e00292cfba50f4d2d
okonomiyakki <83577128+okonomiyakki@users.noreply.github.com>
2022-01-18 12:55:44 +0900

    Update AP_broadcast_LED

--- AP_broadcast/AP_broadcast.ino
+++ AP_broadcast/AP_broadcast.ino
@@ -1,4 +1,4 @@
-//wemos d1 : AP_broadcast
+//wemos d1 : AP_broadcast_LED
 #include <ESP8266WiFi.h>
 #include <WiFiUdp.h>
 
@@ -6,14 +6,14 @@ const char *ssid = "UDP_TEST";
 const char *pass = "password"; 
 
 IPAddress broadcastIP(192,168,4,255);
-           
 unsigned int localPort = 8266;
-                          
-                              
-
+                                                      
 WiFiUDP udp;
 
 void setup() {
+
+  pinMode(0, INPUT); // D8
+  
   Serial.begin(9600);
   Serial.println();
   WiFi.softAP(ssid, pass);
@@ -22,22 +22,14 @@ void setup() {
 }
 
 void loop() {
-  int r = udp.parsePacket();
-    if (!r){
-        if(Serial.available()>0){
-        udp.beginPacket(broadcastIP, 8266);
-        
-        char a[1];
-        a[0]=char(Serial.read()); 
-        udp.write(a,1); 
-        udp.endPacket();
-        
-        }
-    }
-    //Serial.println(WiFi.RSSI());
-    
-    
-    //delay(3000);
+  int buttonState = digitalRead(0);
+  
+  udp.beginPacket(broadcastIP, 8266);
+  udp.write(buttonState); 
+  udp.endPacket();
+  delay(100);
+  
+  Serial.println(WiFi.RSSI());     
+  delay(500);
+  
 }
-
- 
