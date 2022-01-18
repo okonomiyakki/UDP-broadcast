c491302f5e063e400c099ed0ef70033e7589a840
okonomiyakki <83577128+okonomiyakki@users.noreply.github.com>
2022-01-18 12:58:38 +0900

    Update AP_broadcast_LED from Huskylens

--- AP_broadcast/AP_broadcast.ino
+++ AP_broadcast/AP_broadcast.ino
@@ -1,6 +1,10 @@
-//wemos d1 : AP_broadcast_LED
+//wemos d1 : AP_broadcast_LED & lens
 #include <ESP8266WiFi.h>
 #include <WiFiUdp.h>
+#include <SoftwareSerial.h>
+#define TX 16
+#define RX 14 //13
+SoftwareSerial lens(RX, TX); 
 
 const char *ssid = "UDP_TEST";
 const char *pass = "password"; 
@@ -12,24 +16,35 @@ WiFiUDP udp;
 
 void setup() {
 
-  pinMode(0, INPUT); // D8
+  pinMode(0, INPUT);
+  
+  lens.begin(9600);
   
   Serial.begin(9600);
   Serial.println();
   WiFi.softAP(ssid, pass);
   
   udp.begin(localPort);
+  
 }
 
 void loop() {
-  int buttonState = digitalRead(0);
+
+  int buttonState_1 = lens.read();
+  int buttonState_2 = digitalRead(0);
   
-  udp.beginPacket(broadcastIP, 8266);
-  udp.write(buttonState); 
+  udp.beginPacket(broadcastIP, 8266); 
+  if(buttonState_1 == 0 || buttonState_2 == 0){ 
+      udp.write(8);
+      Serial.println(buttonState_1);
+      Serial.print("   ");
+      Serial.println(buttonState_2); 
+  }
   udp.endPacket();
+  
   delay(100);
-  
-  Serial.println(WiFi.RSSI());     
-  delay(500);
-  
+
+  //Serial.println(WiFi.RSSI());     
+  //delay(500);
+ 
 }
