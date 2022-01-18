e7527ecab644097f75e198910e16078c1cbece3f
okonomiyakki <83577128+okonomiyakki@users.noreply.github.com>
2022-01-18 13:12:18 +0900

    STA_Node_MCU (Receiver)

--- STA_broadcast/STA_broadcast.ino
+++ STA_broadcast/STA_broadcast.ino
@@ -13,13 +13,18 @@ unsigned int localPort = 8266; //포트번호 선언
 WiFiUDP udp; //wifi udp통신 시작
 
 void setup(){
-    pinMode(16, OUTPUT); //내장 led선언
+    pinMode(16, OUTPUT);
+    pinMode(LED_BUILTIN, OUTPUT); 
     
     Serial.begin(9600);
     Serial.println();
     WiFi.begin(ssid, pass); //STAION(단말기)모드로 선언하여 해당 핫스팟에 접속  
     Serial.println("");
   while (WiFi.status() != WL_CONNECTED) { 
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(500);
+    Serial.print(".");
+    digitalWrite(LED_BUILTIN, LOW);
     delay(500);
     Serial.print(".");
   }
@@ -35,15 +40,17 @@ void setup(){
    
 }
 void loop(){
-  
+    
     udp.parsePacket();
-    int packetBuffer = udp.read(); //wifi로 수신된 값을 변수에 저장
-    int data = packetBuffer;
-    if(data == 8) 
+    
+    int data = udp.read(); //wifi로 수신된 값을 변수에 저장
+    
+    if(data == 0) 
        digitalWrite(16, HIGH); //수신된 값이 송신한 값과 일치한다면 'ON'
     else
        digitalWrite(16, LOW); //수신이 안된 상황 혹은 송신중 발생한 에러값을 받으면 'OFF'
-    Serial.print(data); 
+    Serial.println(data); 
+    
     delay(100); 
    
 }
